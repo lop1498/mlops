ae98a98abc88defe8a94239cdbdb56fee41d8424
Pol Garcia Recasens <polgarciarecasens@gmail.com>
2023-01-11 19:28:56 +0100

    cleaning code style

--- session2/project/src/models/baseline.py
+++ session2/project/src/models/baseline.py
@@ -3,19 +3,19 @@ import torch.nn.functional as F
 
 
 class Baseline(nn.Module):
-  def __init__(self, size, classes):
-    super().__init__()
-    self.size = size
-    self.classes = classes
-    self.fc1 = nn.Linear(self.size, 512)
-    self.fc2 = nn.Linear(512, 256)
-    self.fc3 = nn.Linear(256, 128)
-    self.fc4 = nn.Linear(128, self.classes)
+    def __init__(self, size, classes):
+        super().__init__()
+        self.size = size
+        self.classes = classes
+        self.fc1 = nn.Linear(self.size, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, self.classes)
 
-  def forward(self, x):
-    x = F.relu(self.fc1(x))
-    x = F.relu(self.fc2(x))
-    x = F.relu(self.fc3(x))
-    x = F.relu(self.fc4(x))
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
 
-    return x
+        return x
--- session2/project/src/models/train_model.py
+++ session2/project/src/models/train_model.py
@@ -8,45 +8,45 @@ import numpy as np
 @click.command()
 @click.argument('path_data', type=click.Path(exists=True))
 def main(path_data):
-  # Load the train data and labels
-  train_data = torch.tensor(np.load(path_data+'/train_data.npy'), dtype=torch.float)
-  train_labels = torch.tensor(np.load(path_data+'/train_labels.npy'))
+    # Load the train data and labels
+    train_data = torch.tensor(np.load(path_data+'/train_data.npy'), dtype=torch.float)
+    train_labels = torch.tensor(np.load(path_data+'/train_labels.npy'))
 
-  # Create a TensorDataset from the train data and labels
-  train_dataset = TensorDataset(train_data, train_labels)
+    # Create a TensorDataset from the train data and labels
+    train_dataset = TensorDataset(train_data, train_labels)
 
-  # Create a DataLoader from the train dataset
-  train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    # Create a DataLoader from the train dataset
+    train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
-  # Load the test data and labels
-  test_data = torch.tensor(np.load(path_data+'/test_data.npy'), dtype=torch.float)
-  test_labels = torch.tensor(np.load(path_data+'/test_labels.npy'))
+    # Load the test data and labels
+    test_data = torch.tensor(np.load(path_data+'/test_data.npy'), dtype=torch.float)
+    test_labels = torch.tensor(np.load(path_data+'/test_labels.npy'))
 
-  # Create a TensorDataset from the test data and labels
-  test_dataset = TensorDataset(test_data, test_labels)
+    # Create a TensorDataset from the test data and labels
+    # test_dataset = TensorDataset(test_data, test_labels)
 
-  # Create a DataLoader from the test dataset
-  test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+    # Create a DataLoader from the test dataset
+    # test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=True)
 
-  model = Baseline(784, 10)
-  optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
-  criterion = torch.nn.CrossEntropyLoss()
+    model = Baseline(784, 10)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+    criterion = torch.nn.CrossEntropyLoss()
 
-  epochs = 5
-  for epoch in range(epochs):
-    loss_epoch = 0
-    for images, labels in train_dataloader:
-      optimizer.zero_grad()
-      pred = model(images)
-      loss = criterion(pred, labels)
-      loss.backward()
-      optimizer.step()
-      loss_epoch += loss.item()
-    print(f"Training loss on epoch {epoch}: {loss_epoch/len(train_dataloader)}")
-  
-  torch.save(model.state_dict(), 'models/model_epoch{}.pth'.format(epochs))
+    epochs = 5
+    for epoch in range(epochs):
+        loss_epoch = 0
+        for images, labels in train_dataloader:
+            optimizer.zero_grad()
+            pred = model(images)
+            loss = criterion(pred, labels)
+            loss.backward()
+            optimizer.step()
+            loss_epoch += loss.item()
+        print(f"Training loss on epoch {epoch}: {loss_epoch/len(train_dataloader)}")
 
-if __name__=="__main__":
-  #path_data = '/data/processed'
-  main()
+    torch.save(model.state_dict(), 'models/model_epoch{}.pth'.format(epochs))
 
+
+if __name__ == "__main__":
+    # path_data = '/data/processed'
+    main()
